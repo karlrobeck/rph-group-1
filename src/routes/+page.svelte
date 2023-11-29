598e1aca98b83a55cd37b9d7ef4bfa7e67e5562c
karl robeck alferez <karlalferezfx@gmail.com>
2023-11-29 18:54:57 +0800

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,195 @@
+<!-- Hero -->
+<div class="bg-slate-900 h-full">
+	<div class="bg-gradient-to-b from-violet-600/[.15] via-transparent">
+		<div
+			class="flex flex-col justify-center items-center max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 p-24 space-y-8"
+		>
+			<!-- Title -->
+			<div class="max-w-3xl text-center mx-auto">
+				<h1 class="block font-medium text-gray-200 text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+					LAND REFORMS
+				</h1>
+			</div>
+			<!-- End Title -->
+
+			<div class="max-w-3xl text-center mx-auto">
+				<p class="text-lg text-gray-400">Reading the Philippine History / Group 1 presentation</p>
+				<p class="text-lg text-gray-400 font-bold">BS301A</p>
+			</div>
+
+			<!-- Buttons -->
+			<!-- Card Blog -->
+			<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+				<!-- Grid -->
+				<div class="grid lg:grid-cols-2 lg:gap-y-16 gap-10">
+					<!-- Card -->
+					<a
+						class="group rounded-xl overflow-hidden focus:outline-none focus:ring-1 focus:ring-gray-600"
+						href="/barlaan"
+					>
+						<div class="sm:flex">
+							<div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
+								<img
+									class="group-hover:scale-105 transition-transform duration-500 ease-in-out w-full h-full absolute top-0 start-0 object-cover rounded-xl"
+									src="https://miro.medium.com/v2/resize:fit:1400/1*Xy1Iac9K_UCZ5Fdxa_P1Ow.jpeg"
+									alt="Spanish Colonization Era"
+								/>
+							</div>
+
+							<div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
+								<h3 class="text-xl font-semibold text-gray-300 group-hover:text-white">
+									Andrei Barlaan
+								</h3>
+								<p class="mt-3 text-gray-400">Spanish Colonization Era</p>
+								<p
+									class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium"
+								>
+									Read more
+									<svg
+										class="flex-shrink-0 w-4 h-4"
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+										fill="none"
+										stroke="currentColor"
+										stroke-width="2"
+										stroke-linecap="round"
+										stroke-linejoin="round"><path d="m9 18 6-6-6-6" /></svg
+									>
+								</p>
+							</div>
+						</div>
+					</a>
+					<!-- End Card -->
+
+					<!-- Card -->
+					<a
+						class="group rounded-xl overflow-hidden focus:outline-none focus:ring-1 focus:ring-gray-600"
+						href="/ardonia"
+					>
+						<div class="sm:flex">
+							<div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
+								<img
+									class="group-hover:scale-105 transition-transform duration-500 ease-in-out w-full h-full absolute top-0 start-0 object-cover rounded-xl"
+									src="https://esielcabrera.files.wordpress.com/2016/02/localtourists_eandacay_02.jpg?w=659"
+									alt="Pre Colonization Era"
+								/>
+							</div>
+
+							<div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
+								<h3 class="text-xl font-semibold text-gray-300 group-hover:text-white">
+									Ralph Ardonia
+								</h3>
+								<p class="mt-3 text-gray-400">Pre Colonization Era</p>
+								<p
+									class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium"
+								>
+									Read more
+									<svg
+										class="flex-shrink-0 w-4 h-4"
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+										fill="none"
+										stroke="currentColor"
+										stroke-width="2"
+										stroke-linecap="round"
+										stroke-linejoin="round"><path d="m9 18 6-6-6-6" /></svg
+									>
+								</p>
+							</div>
+						</div>
+					</a>
+					<!-- End Card -->
+
+					<!-- Card -->
+					<a
+						class="group rounded-xl overflow-hidden focus:outline-none focus:ring-1 focus:ring-gray-600"
+						href="/alferez"
+					>
+						<div class="sm:flex">
+							<div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
+								<img
+									class="group-hover:scale-105 transition-transform duration-500 ease-in-out w-full h-full absolute top-0 start-0 object-cover rounded-xl"
+									src="https://qph.cf2.quoracdn.net/main-qimg-a8d85a0d52081eb9dfa3d99ba9df37ea-lq"
+									alt="American Colonization Era"
+								/>
+							</div>
+
+							<div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
+								<h3 class="text-xl font-semibold text-gray-300 group-hover:text-white">
+									Karl Robeck Alferez
+								</h3>
+								<p class="mt-3 text-gray-400">American Colonization Era</p>
+								<p
+									class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium"
+								>
+									Read more
+									<svg
+										class="flex-shrink-0 w-4 h-4"
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+										fill="none"
+										stroke="currentColor"
+										stroke-width="2"
+										stroke-linecap="round"
+										stroke-linejoin="round"><path d="m9 18 6-6-6-6" /></svg
+									>
+								</p>
+							</div>
+						</div>
+					</a>
+					<!-- End Card -->
+
+					<!-- Card -->
+					<a
+						class="group rounded-xl overflow-hidden focus:outline-none focus:ring-1 focus:ring-gray-600"
+						href="/trogo"
+					>
+						<div class="sm:flex">
+							<div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
+								<img
+									class="group-hover:scale-105 transition-transform duration-500 ease-in-out w-full h-full absolute top-0 start-0 object-cover rounded-xl"
+									src="https://www.nationalww2museum.org/sites/default/files/2020-09/060320-F-1234P-001.JPG.jpg"
+									alt="Japanese Colonization Era"
+								/>
+							</div>
+
+							<div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
+								<h3 class="text-xl font-semibold text-gray-300 group-hover:text-white">
+									Clark Trogo
+								</h3>
+								<p class="mt-3 text-gray-400">Japanese Era</p>
+								<p
+									class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium"
+								>
+									Read more
+									<svg
+										class="flex-shrink-0 w-4 h-4"
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+										fill="none"
+										stroke="currentColor"
+										stroke-width="2"
+										stroke-linecap="round"
+										stroke-linejoin="round"><path d="m9 18 6-6-6-6" /></svg
+									>
+								</p>
+							</div>
+						</div>
+					</a>
+					<!-- End Card -->
+				</div>
+				<!-- End Grid -->
+			</div>
+			<!-- End Card Blog -->
+		</div>
+	</div>
+</div>
+<!-- End Hero -->
---
+++ src/routes/[user]/+page.svelte
@@ -0,0 +1,141 @@
+<script>
+	import showdown from 'showdown';
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+
+	const converter = new showdown.Converter();
+	converter.setFlavor('github');
+
+	let document;
+	onMount(async () => {
+		const res = await fetch(`/${$page.params.user}.md`);
+		document = converter.makeHtml(await res.text());
+	});
+</script>
+
+<svelte:head>
+	<title>{$page.params.user} Blog</title>
+</svelte:head>
+
+<!-- Blog Article -->
+<main class="w-full dark:bg-gray-900 h-screen">
+	<div class="h-full max-w-3xl px-4 pt-6 lg:pt-10 pb-12 sm:px-6 lg:px-8 mx-auto">
+		<div class="max-w-2xl">
+			<a
+				class="pb-4 inline-flex items-center gap-x-1.5 text-sm text-gray-600 decoration-2 hover:underline dark:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+				href="/"
+			>
+				<svg
+					class="flex-shrink-0 w-4 h-4"
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"><path d="m15 18-6-6 6-6" /></svg
+				>
+				Back to Blog
+			</a>
+			<!-- Avatar Media -->
+			<div class="flex justify-between items-center mb-6">
+				<div class="flex w-full sm:items-center gap-x-5 sm:gap-x-3">
+					<div class="flex-shrink-0">
+						{#if $page.params.user === 'ardonia'}
+							<img
+								class="h-12 w-12 rounded-full object-cover"
+								src="https://scontent.fmnl25-3.fna.fbcdn.net/v/t39.30808-1/340242717_6033317763384716_670218383113599144_n.jpg?stp=dst-jpg_p200x200&_nc_cat=106&ccb=1-7&_nc_sid=5740b7&_nc_eui2=AeE7xeoyLJXSHoCVbO_GMFCgX5UiGWLoIgxflSIZYugiDMC-s3zt5gcVGn8RnpVPGq7YRqpHCmvLKPkavy5pCqeF&_nc_ohc=dbf0rXK1Hn0AX9wVm2p&_nc_ht=scontent.fmnl25-3.fna&oh=00_AfCsf6xWKEqxq5QxQ_QnSR3wCnRjXzcfg3k90xuHe7ZwrA&oe=656C56CC"
+								alt={$page.params.user}
+							/>
+						{:else if $page.params.user === 'barlaan'}
+							<img
+								class="h-12 w-12 rounded-full object-cover"
+								src="https://scontent.fmnl25-3.fna.fbcdn.net/v/t1.6435-9/176738317_2107970502729936_3272826855005610149_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=73878a&_nc_eui2=AeFQMlws_IaA5oVJHVbEQ3tDU_9V9wubSxhT_1X3C5tLGNEUroRwNY1DKg6vIHEF8sK_1uMNlaSgLuF-yU_lHYv7&_nc_ohc=KtkpAxXNnw8AX-DW_3j&_nc_ht=scontent.fmnl25-3.fna&oh=00_AfCshjqku3YtX7OCVOHl_KLyN48kjwFG6E1VsxeEDoAj5w&oe=658E67D5"
+								alt={$page.params.user}
+							/>
+						{:else if $page.params.user === 'trogo'}
+							<img
+								class="h-12 w-12 rounded-full object-cover"
+								src="https://scontent.fmnl25-1.fna.fbcdn.net/v/t1.6435-9/62253520_2936519453041741_8277062214649643008_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=7a1959&_nc_eui2=AeH_PYSvxH0gEm-itJtz7cFztZ58i7RIblW1nnyLtEhuVcpTF0hLeXA_MTmfwBSXkH3yIqwnQmt2ddQYVwrfJTeU&_nc_ohc=IMSt3BbjJ-EAX8XWRDR&_nc_ht=scontent.fmnl25-1.fna&oh=00_AfACFkbJZaAPvxexhakLs6JSK0iJWy61Q0rB3QcEQ7wBIA&oe=658E7A0A"
+								alt={$page.params.user}
+							/>
+						{:else if $page.params.user === 'alferez'}
+							<img
+								class="h-12 w-12 rounded-full"
+								src="https://scontent.fmnl25-4.fna.fbcdn.net/v/t39.30808-6/316550295_1007028853591272_3425713151920287560_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=9c7eae&_nc_eui2=AeFPo5FUOmQUZJJLFBVDK21F0ixDpE5kRbDSLEOkTmRFsOu0RN08ThrTQlv95Xz1x-Z2tiVQB9cLO5SNRehpzWPD&_nc_ohc=MbaS36yGZewAX9m6cGa&_nc_ht=scontent.fmnl25-4.fna&oh=00_AfC35OWIyRt9ROrHMycOfJ001E_DXSRgRwmnSgQttglFzg&oe=656B5906"
+								alt={$page.params.user}
+							/>
+						{/if}
+					</div>
+					<div class="grow">
+						<div class="flex justify-between items-center gap-x-2">
+							<div>
+								<!-- Tooltip -->
+								<div class="hs-tooltip inline-block [--trigger:hover] [--placement:bottom]">
+									<div class="hs-tooltip-toggle sm:mb-1 block text-start cursor-pointer">
+										<span class="font-semibold text-gray-800 dark:text-gray-200">
+											{#if $page.params.user === 'ardonia'}
+												Ralph Ardonia
+											{:else if $page.params.user === 'barlaan'}
+												Andrei Barlaan
+											{:else if $page.params.user === 'trogo'}
+												Clark Trogo
+											{:else if $page.params.user === 'alferez'}
+												Karl Robeck Alferez
+											{/if}
+										</span>
+									</div>
+								</div>
+								<!-- End Tooltip -->
+
+								<ul class="text-xs text-gray-500">
+									<li
+										class="inline-block relative pe-6 last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-2 before:-translate-y-1/2 before:w-1 before:h-1 before:bg-gray-300 before:rounded-full dark:text-gray-400 dark:before:bg-gray-600"
+									>
+										November 30, 2023
+									</li>
+								</ul>
+							</div>
+							<!-- Button Group -->
+							<div>
+								<a
+									href="https://www.facebook.com/ralphlauren.ardona"
+									target="_blank"
+									type="button"
+									class="py-1.5 px-2.5 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="16"
+										height="16"
+										fill="currentColor"
+										class="bi bi-facebook"
+										viewBox="0 0 16 16"
+									>
+										<path
+											d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"
+										/>
+									</svg>
+									Facebook
+								</a>
+							</div>
+							<!-- End Button Group -->
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- End Avatar Media -->
+
+			<!-- Content -->
+			<div class="prose dark:prose-invert">
+				{#if document}
+					{@html document}
+				{/if}
+			</div>
+			<!-- End Content -->
+		</div>
+	</div>
+</main>
+<!-- End Blog Article -->
